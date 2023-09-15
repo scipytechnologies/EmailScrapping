7f24d6c6405df9c379700af2337b286d9d35e2b5
unknown <vishva.scipy@gmail.com>
2023-09-15 11:39:25 +0530

    update multiple url

--- client/src/pages/Multipleurl/Multiurl.jsx
+++ client/src/pages/Multipleurl/Multiurl.jsx
@@ -1,199 +1,272 @@
+// import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import * as XLSX from 'xlsx';
+
+// function MultiScrap() {
+//   const [file, setFile] = useState(null);
+//   const [emailData, setEmailData] = useState([]);
+//   const [error, setError] = useState('');
+
+//   const handleFileChange = (e) => {
+//     const selectedFile = e.target.files[0];
+//     setFile(selectedFile);
+//   };
+
+//   const handleScrap = async () => {
+//     if (!file) {
+//       setError('Please upload a file.');
+//       return;
+//     }
+
+//     try {
+//       const fileContents = await readFileContents(file);
+//       const urls = fileContents.split('\n').filter(Boolean);
+
+//       const response = await fetch('http://localhost:8000/scrape/multipleurl', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ urls }),
+//       });
+
+//       if (response.ok) {
+//         const data = await response.json();
+//         setEmailData(data.emailData);
+//         setError('');
+//       } else {
+//         setError('Error extracting emails');
+//       }
+//     } catch (error) {
+//       setError('Internal server error');
+//     }
+//   };
+
+//   const handleDownload = () => {
+//     if (emailData.length > 0) {
+//       const csvData = [["URL", "Email"]];
+    
+//       emailData.forEach((item) => {
+//         const url = item.url;
+//         if (item.emails.length === 0) {
+//           // If there are no emails for this URL, just add the URL row
+//           csvData.push([url, '']);
+//         } else {
+//           // Add the URL once followed by its associated emails
+//           csvData.push([url, item.emails[0]]); // Add the first email
+//           for (let i = 1; i < item.emails.length; i++) {
+//             csvData.push(['', item.emails[i]]); // Add subsequent emails with an empty URL cell
+//           }
+//         }
+//       });
+    
+//       const csvContent = csvData.map((row) => row.join(',')).join('\n');
+//       const dataURI = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
+    
+//       const a = document.createElement('a');
+//       a.href = dataURI;
+//       a.download = 'email_data.csv';
+//       a.click();
+//     }
+//   };
+  
+  
+  
+
+//   const readFileContents = (file) => {
+//     return new Promise((resolve, reject) => {
+//       const reader = new FileReader();
+//       reader.onload = (event) => {
+//         const contents = event.target.result;
+//         resolve(contents);
+//       };
+//       reader.onerror = (error) => {
+//         reject(error);
+//       };
+//       reader.readAsText(file);
+//     });
+//   };
+
+//   return (
+//     <div className="container mt-5">
+//       <h1>Multiple Email Scraper</h1>
+//       <form className="mb-3">
+//         <label htmlFor="urlInput" className="form-label">
+//           Upload a Text File:
+//         </label>
+//         <input className="form-control" id="urlInput" type="file" accept=".txt" onChange={handleFileChange} />
+//       </form>
+//       <div className="mb-3">
+//         <button className="btn btn-primary" onClick={handleScrap}>
+//           Scrap Emails
+//         </button>
+//         <button className="btn btn-success" onClick={handleDownload} disabled={emailData.length === 0}>
+//           Download CSV
+//         </button>
+//       </div>
+//       {error && <p className="error">{error}</p>}
+//       <div className="result">
+//         {emailData.length > 0 && (
+//           <div className="mt-4">
+//             <h2>Extracted Emails</h2>
+//             <div className="email-container">
+//               <ul>
+//                 {emailData.map((item, index) => (
+//                   <li key={index}>
+//                     <strong>URL:</strong> {item.url}
+//                     <ul>
+//                       {item.emails.map((email, i) => (
+//                         <li key={i}>{email}</li>
+//                       ))}
+//                     </ul>
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default MultiScrap;
+
+
+
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-import xlsx from 'json-as-xlsx';
+import * as XLSX from 'xlsx';
 
 function MultiScrap() {
-  const [fileContent, setFileContent] = useState('');
-  const [uniqueEmails, setUniqueEmails] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [err, setErr] = useState(false);
+  const [file, setFile] = useState(null);
+  const [emailData, setEmailData] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false); // New loading state
 
-  const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    if (selectedFile) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setFileContent(e.target.result);
-      };
-      reader.readAsText(selectedFile);
-    }
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
   };
 
-  const handleScrapeEmails = async () => {
-    setLoading(true);
-    setErr(false);
+  const handleScrap = async () => {
+    if (!file) {
+      setError('Please upload a file.');
+      return;
+    }
 
     try {
-      const response = await axios.post(
-        'http://localhost:8000/scrape/multipleurl',
-        { file: fileContent },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+      setLoading(true); // Set loading to true when scraping begins
 
-      setUniqueEmails(response.data.emailLinks);
-    } catch (error) {
-      console.error(error);
-      setErr(true);
-    } finally {
-      setLoading(false);
-    }
-  };
+      const fileContents = await readFileContents(file);
+      const urls = fileContents.split('\n').filter(Boolean);
 
-  const handleExportToExcel = () => {
-    if (uniqueEmails.length > 0) {
-      const data = [
-        {
-          sheet: 'EmailLinks',
-          columns: [{ label: 'Email Links', value: 'email' }],
-          content: uniqueEmails.map((email) => ({ email })),
+      const response = await fetch('http://localhost:8000/scrape/multipleurl', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
         },
-      ];
+        body: JSON.stringify({ urls }),
+      });
 
-      const settings = {
-        fileName: 'EmailLinksExport',
-        extraLength: 3,
-      };
-
-      xlsx(data, settings);
+      if (response.ok) {
+        const data = await response.json();
+        setEmailData(data.emailData);
+        setError('');
+      } else {
+        setError('Error extracting emails');
+      }
+    } catch (error) {
+      setError('Internal server error');
+    } finally {
+      setLoading(false); // Set loading back to false when scraping completes
     }
   };
 
-  const loaderBtn = () => {
-    return (
-      <button className="btn btn-primary" type="button" disabled>
-        <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-        Loading...
-      </button>
-    );
+  const handleDownload = () => {
+    if (emailData.length > 0) {
+      const csvData = [["URL", "Email"]];
+
+      emailData.forEach((item) => {
+        const url = item.url;
+        if (item.emails.length === 0) {
+          // If there are no emails for this URL, just add the URL row
+          csvData.push([url, '']);
+        } else {
+          // Add the URL once followed by its associated emails
+          csvData.push([url, item.emails[0]]); // Add the first email
+          for (let i = 1; i < item.emails.length; i++) {
+            csvData.push(['', item.emails[i]]); // Add subsequent emails with an empty URL cell
+          }
+        }
+      });
+
+      const csvContent = csvData.map((row) => row.join(',')).join('\n');
+      const dataURI = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvContent);
+
+      const a = document.createElement('a');
+      a.href = dataURI;
+      a.download = 'email_data.csv';
+      a.click();
+    }
+  };
+
+  const readFileContents = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const contents = event.target.result;
+        resolve(contents);
+      };
+      reader.onerror = (error) => {
+        reject(error);
+      };
+      reader.readAsText(file);
+    });
   };
 
   return (
     <div className="container mt-5">
-      <h1>Email Scraper</h1>
-      <div className="input-group mb-3">
-        <label className="input-group-text" htmlFor="inputGroupFile01">
-          Upload
+      <h1>Multiple Email Scraper</h1>
+      <form className="mb-3">
+        <label htmlFor="urlInput" className="form-label">
+          Upload a Text File:
         </label>
-        <input type="file" className="form-control" id="inputGroupFile01" onChange={handleFileChange}></input>
-      </div>
-      {loading ? (
-        loaderBtn()
-      ) : (
-        <button className="btn btn-primary" onClick={handleScrapeEmails} disabled={!fileContent}>
-          Scrape Emails
+        <input className="form-control" id="urlInput" type="file" accept=".txt" onChange={handleFileChange} />
+      </form>
+      <div className="mb-3">
+        <button className="btn btn-primary" onClick={handleScrap} disabled={loading}>
+          {loading ? 'Loading...' : 'Scrap Emails'}
         </button>
-      )}
-
-      <button className="btn btn-success ms-2" onClick={handleExportToExcel} disabled={!uniqueEmails.length}>
-        Export to Excel
-      </button>
-
-      <div className="mt-4">
-        <h3>Scraped Emails:</h3>
-        <div className="email-container">
-          {uniqueEmails.length > 0 ? (
-            uniqueEmails.map((uniqueEmail, index) => (
-              <span key={index}>{uniqueEmail}<br /></span>
-            ))
-          ) : (
-            <p>{err && "No emails found try again"}</p>
-          )}
-        </div>
+        <button className="btn btn-success" onClick={handleDownload} disabled={emailData.length === 0}>
+          Download CSV
+        </button>
       </div>
+      {loading && <div className="loader"></div>} {/* Loader */}
+      {!loading && error && <p className="error">{error}</p>} {/* Error message */}
+      {!loading && emailData.length > 0 && (
+        <div className="result">
+          <div className="mt-4">
+            <h2>Extracted Emails</h2>
+            <div className="email-container">
+              <ul>
+                {emailData.map((item, index) => (
+                  <li key={index}>
+                    <strong>URL:</strong> {item.url}
+                    <ul>
+                      {item.emails.map((email, i) => (
+                        <li key={i}>{email}</li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default MultiScrap;
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import axios from 'axios';
-
-// function MultiScrap() {
-//     const [fileContent, setFileContent] = useState('');
-//     const [uniqueEmails, setUniqueEmails] = useState([]);
-//     const [loading, setLoading] = useState(false);
-//     const [err, setErr] = useState(false);
-
-//     const handleFileChange = (event) => {
-//         const selectedFile = event.target.files[0];
-//         if (selectedFile) {
-//             const reader = new FileReader();
-//             reader.onload = (e) => {
-//                 setFileContent(e.target.result);
-//             };
-//             reader.readAsText(selectedFile);
-//         }
-//     };
-
-//     const handleScrapeEmails = async () => {
-//         setLoading(true);
-//         setErr(false);
-
-//         try {
-//             const response = await axios.post('http://localhost:8000/scrape/multipleurl', { file: fileContent }, {
-//                 headers: {
-//                     'Content-Type': 'application/json', // Update content type
-//                 },
-//             });
-
-//             setUniqueEmails(response.data.emailLinks);
-//         } catch (error) {
-//             console.error(error);
-//             setErr(true);
-//         } finally {
-//             setLoading(false);
-//         }
-//     };
-
-//     const loaderBtn = () => {
-//         return (
-//             <button className="btn btn-primary" type="button" disabled>
-//                 <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-//                 Loading...
-//             </button>
-//         );
-//     };
-
-//     return (
-//         <div className="container mt-5">
-//             <h1>Email Scraper</h1>
-//             <div className="input-group mb-3">
-//                 <label className="input-group-text" htmlFor="inputGroupFile01">Upload</label>
-//                 <input type="file" className="form-control" id="inputGroupFile01" onChange={handleFileChange}></input>
-//             </div>
-//             {
-//                 loading ? loaderBtn() :
-//                     <button className="btn btn-primary" onClick={handleScrapeEmails} disabled={!fileContent}>
-//                         Scrape Emails
-//                     </button>
-//             }
-
-//             <div className="mt-4">
-//                 <h3>Scraped Emails:</h3>
-//                 <div className='email-container'>
-//                     {uniqueEmails.length > 0 ? (
-//                         uniqueEmails.map((uniqueEmail, index) => (
-//                             <span key={index}>{uniqueEmail}<br /></span>
-//                         ))
-//                     ) : (
-//                         <p>{err && "No emails found try again"}</p>
-//                     )}
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default MultiScrap;
--- server/controllers/scrapeController.js
+++ server/controllers/scrapeController.js
@@ -1,8 +1,5 @@
-import puppeteer from "puppeteer";
-import { Single } from "../model/urlSchema.js";
 import axios from "axios";
 import cheerio from "cheerio";
-import emailExtractor from "node-email-extractor";
 
 export const singleurlController = async (req, res) => {
   try {
@@ -56,7 +53,7 @@ export const singleurlController = async (req, res) => {
       // Convert the Set of email addresses to an array
       const emailsArray = Array.from(extractedEmails);
 
-      res.status(200).json({emailLinks: emailsArray });
+      res.status(200).json({ emailLinks: emailsArray });
     } else {
       res.status(response.status).json({ error: "Failed to fetch the page" });
     }
@@ -66,115 +63,84 @@ export const singleurlController = async (req, res) => {
   }
 };
 
-import express from "express";
-import bodyParser from "body-parser";
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+
 
 export const multipleurlController = async (req, res) => {
   try {
-    const fileContent = req.body.file; // Corrected to access the uploaded file content
-    const urls = fileContent.split("\n");
-    console.log(urls);
+    const { urls } = req.body;
 
-    const browser = await puppeteer.launch();
-    const scrapedEmails = new Set();
+    // Validate the URLs array
+    if (!urls || !Array.isArray(urls) || urls.length === 0) {
+      return res.status(400).json({ error: 'Invalid URLs array' });
+    }
 
-    const blockedDomains = [
-      "www.facebook.com",
-      "linkedin.com",
-      "instagram.com",
-      "youtube.com",
-      "tel",
-      "twitter.com",
-    ];
-    const blockedPaths = ["/tel:"];
+    const emailData = [];
 
-    for (const url of urls) {
-      const visitedLinks = new Set();
-      const allEmailLinks = [];
+    // Define a function to process a single URL and extract emails
+    async function processUrl(url) {
+      try {
+        const response = await axios.get(url);
 
-      const mainPage = await browser.newPage();
-      await mainPage.goto(url, {
-        waitUntil: "domcontentloaded",
-      });
+        if (response.status === 200) {
+          const $ = cheerio.load(response.data);
+          const baseUrl = new URL(url);
+          const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+          const extractedEmails = new Set();
 
-      const pageLinks = await mainPage.evaluate(() => {
-        const links = Array.from(document.querySelectorAll("a"));
-        return links.map((link) => link.href);
-      });
-
-      for (const link of pageLinks) {
-        if (
-          !visitedLinks.has(link) &&
-          !blockedDomains.some((domain) => link.includes(domain)) &&
-          !blockedPaths.some((path) => link.includes(path)) &&
-          !link.includes("#") && // Exclude URLs with fragments
-          !/\b\d{10,}\b/.test(link) // Exclude URLs with phone numbers
-        ) {
-          visitedLinks.add(link);
-          console.log(link);
-          if (link.includes("mailto:")) {
-            const email = link.replace("mailto:", "");
-            if (!scrapedEmails.has(email)) {
-              scrapedEmails.add(email);
-              allEmailLinks.push(email);
+          // Extract unique links using Cheerio selectors
+          const uniqueLinks = new Set();
+          $("a").each((index, element) => {
+            const link = $(element).attr("href");
+            if (link && !link.startsWith("#")) {
+              const absoluteLink = new URL(link, baseUrl);
+              if (absoluteLink.hostname === baseUrl.hostname) {
+                uniqueLinks.add(absoluteLink.href);
+              }
             }
-          } else if (link.startsWith("http") || link.startsWith("www")) {
-            let newPage;
+          });
 
+          // Define a function to process links concurrently
+          async function processLink(link) {
             try {
-              newPage = await browser.newPage();
-              await newPage.goto(link, {
-                waitUntil: "domcontentloaded",
+              const response = await axios.get(link);
+              const htmlContent = response.data;
+              const extractedEmailsFromUrl = htmlContent.match(emailRegex) || [];
+              extractedEmailsFromUrl.forEach((email) => {
+                extractedEmails.add(email);
               });
-
-              const normalizedNewPageURL = new URL(newPage.url());
-              const normalizedLink = new URL(link);
-
-              if (normalizedNewPageURL.href === normalizedLink.href) {
-                const pageEmailLinks = await newPage.evaluate(() => {
-                  const links = Array.from(
-                    document.querySelectorAll('a[href^="mailto:"]')
-                  );
-                  return links.map((link) =>
-                    link.getAttribute("href").replace("mailto:", "")
-                  );
-                });
-
-                for (const email of pageEmailLinks) {
-                  if (!scrapedEmails.has(email)) {
-                    scrapedEmails.add(email);
-                    allEmailLinks.push(email);
-                  }
-                }
-              }
             } catch (error) {
-              console.error(`Error while processing link ${link}:`, error);
-            } finally {
-              if (newPage) {
-                await newPage.close();
-              }
+              console.error("Error extracting email addresses from", link, ":", error.message);
             }
           }
+
+          // Process links concurrently using Promise.all
+          const linkPromises = Array.from(uniqueLinks).map(processLink);
+
+          await Promise.all(linkPromises);
+
+          // Convert the Set of extracted email addresses to an array for this URL
+          const emailsArray = Array.from(extractedEmails);
+
+          // Add the URL and its extracted emails to the emailData array
+          emailData.push({ url, emails: emailsArray });
+        } else {
+          console.error("Failed to fetch the page:", url);
         }
+      } catch (error) {
+        console.error("Error processing URL", url, ":", error.message);
       }
-
-      await mainPage.close();
     }
 
-    const uniqueEmails = [...scrapedEmails];
-    if (uniqueEmails.length > 0) {
-      res.json({ emailLinks: uniqueEmails });
-    } else {
-      res.json({ message: "No email links found." });
+    // Process each URL sequentially
+    for (const url of urls) {
+      await processUrl(url);
     }
 
-    await browser.close();
+    res.status(200).json({ emailData });
   } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "An error occurred while scraping." });
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
