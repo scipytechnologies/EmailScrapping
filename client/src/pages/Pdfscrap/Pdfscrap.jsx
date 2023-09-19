0521df5f925777cc2372404762505b03659ad0de
unknown <vishva.scipy@gmail.com>
2023-09-19 13:08:09 +0530

    email not completed

--- client/src/pages/Pdfscrap/Pdfscrap.jsx
+++ client/src/pages/Pdfscrap/Pdfscrap.jsx
@@ -1,79 +1,169 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import axios from 'axios';
+
+// function Pdfscrap() {
+//     const [url, setUrl] = useState('');
+//     const [pdfLinks, setPdfLinks] = useState([]);
+//     const [message, setMessage] = useState('');
+//     const [loading, setLoading] = useState(false);
+//     const [err, setErr] = useState(false);
+
+//     const handleScrapePDFs = async () => {
+//         setLoading(true);
+//         setErr(false);
+//         setMessage('');
+
+//         const body = {
+//             'url': url
+//         };
+
+//         try {
+//             const response = await axios.post('http://localhost:8000/scrape/pdfscrap', body);
+//             const data = response.data;
+
+//             if (data.pdfLinks) {
+//                 setPdfLinks(data.pdfLinks);
+//                 if (data.pdfLinks.length === 0) {
+//                     setMessage('No PDF links found.');
+//                 }
+//             } else if (data.message) {
+//                 setMessage(data.message);
+//             }
+//         } catch (error) {
+//             console.error(error);
+//             setMessage('An error occurred while scraping.');
+//             setErr(true);
+//         } finally {
+//             setLoading(false);
+//         }
+//     };
+
+//     return (
+//         <div className="container mt-5">
+//             <h1>PDF Scraper</h1>
+//             <div className="mb-3">
+//                 <label htmlFor="urlInput" className="form-label">
+//                     Enter URL:
+//                 </label>
+//                 <input
+//                     type="text"
+//                     className="form-control"
+//                     id="urlInput"
+//                     placeholder="eg:- www.example.com"
+//                     value={url}
+//                     onChange={(e) => setUrl(e.target.value)}
+//                     required
+//                 />
+
+//             </div>
+//             <button
+//                 className="btn btn-primary"
+//                 onClick={handleScrapePDFs}
+//                 disabled={loading}
+//             >
+//                 {loading ? (
+//                     <span>Loading...</span>
+//                 ) : (
+//                     'Scrape PDFs'
+//                 )}
+//             </button>
+//             {pdfLinks.length > 0 && (
+//                 <div className="mt-4">
+//                     <h3>PDF Links:</h3>
+//                     <div className='email-container'>
+//                         <ul>
+//                             {pdfLinks.map((link, index) => (
+//                                 <li key={index}>
+//                                     <a href={link} target="_blank" rel="noopener noreferrer">
+//                                         {link}
+//                                     </a>
+//                                 </li>
+//                             ))}
+//                         </ul>
+//                     </div>
+//                 </div>
+//             )}
+//             {message && <p>{message}</p>}
+//         </div>
+//     );
+// }
+
+// export default Pdfscrap;
+
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+import axios from "axios";
 
-function Pdfscrap() {
-    const [url, setUrl] = useState('');
-    const [pdfLinks, setPdfLinks] = useState([]);
-    const [message, setMessage] = useState('');
+const Pdfscrap = () => {
+    const [url, setUrl] = useState("");
+    const [pdfLinks, setPdfLinks] = useState(new Set());
+    const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [err, setErr] = useState(false);
+    const [pdfsFound, setPdfsFound] = useState(false);
 
-    const handleScrapePDFs = async () => {
+
+    const handleUrlChange = (e) => {
+        setUrl(e.target.value);
+        setPdfsFound(true);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         setLoading(true);
-        setErr(false);
-        setMessage('');
-
-        const body = {
-            'url': url
-        };
-
+    
         try {
-            const response = await axios.post('http://localhost:8000/scrape/pdfscrap', body);
-            const data = response.data;
-
-            if (data.pdfLinks) {
-                setPdfLinks(data.pdfLinks);
-                if (data.pdfLinks.length === 0) {
-                    setMessage('No PDF links found.');
-                }
-            } else if (data.message) {
-                setMessage(data.message);
+            const response = await axios.post("http://localhost:8000/scrape/pdfscrap", { url });
+            const newLinks = response.data.pdfLinks;
+    
+            // Create a new Set with only the new links
+            const newPdfLinks = new Set(newLinks);
+    
+            setPdfLinks(newPdfLinks); // Set the state to the new Set of links
+    
+            if (newLinks.length > 0) {
+                setPdfsFound(true); // PDFs were found
+            } else {
+                setPdfsFound(false); // No PDFs found
             }
-        } catch (error) {
-            console.error(error);
-            setMessage('An error occurred while scraping.');
-            setErr(true);
+    
+            setError(null);
+        } catch (err) {
+            setError("Failed to fetch PDF links.");
         } finally {
             setLoading(false);
         }
     };
+    
 
     return (
         <div className="container mt-5">
-            <h1>PDF Scraper</h1>
+            <h2>PDF Scraper</h2>
             <div className="mb-3">
-                <label htmlFor="urlInput" className="form-label">
-                    Enter URL:
-                </label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="urlInput"
-                    placeholder="eg:- www.example.com"
-                    value={url}
-                    onChange={(e) => setUrl(e.target.value)}
-                    required
-                />
-
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="urlInput" className="form-label">Enter URL:</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="urlInput"
+                        placeholder="eg:- https://www.example.com"
+                        value={url}
+                        onChange={handleUrlChange}
+                        required
+                    />
+                </form>
             </div>
-            <button
-                className="btn btn-primary"
-                onClick={handleScrapePDFs}
-                disabled={loading}
-            >
-                {loading ? (
-                    <span>Loading...</span>
-                ) : (
-                    'Scrape PDFs'
-                )}
+            <button className="btn btn-primary" type="submit" onClick={handleSubmit} disabled={loading}>
+                {loading ? "Scraping..." : "Scrape PDFs"}
             </button>
-            {pdfLinks.length > 0 && (
+            {error && <p className="error">{error}</p>}
+        
+            {pdfLinks.size > 0 && (
                 <div className="mt-4">
                     <h3>PDF Links:</h3>
                     <div className='email-container'>
                         <ul>
-                            {pdfLinks.map((link, index) => (
+                            {[...pdfLinks].map((link, index) => (
                                 <li key={index}>
                                     <a href={link} target="_blank" rel="noopener noreferrer">
                                         {link}
@@ -84,9 +174,9 @@ function Pdfscrap() {
                     </div>
                 </div>
             )}
-            {message && <p>{message}</p>}
+            {!pdfsFound && !loading && url &&  <p className='email-container'>No PDFs found.</p>} 
         </div>
     );
-}
+};
 
 export default Pdfscrap;
--- server/controllers/pdfController.js
+++ server/controllers/pdfController.js
@@ -1,83 +1,125 @@
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
+
+// export const scrapePdfsController = async (req, res) => {
+//   const { url } = req.body;
+//   console.log("url", url);
+
+//   try {
+//     const browser = await puppeteer.launch();
+//     const visitedLinks = new Set();
+//     const pdfLinks = new Set();
+
+//     const blockedDomains = ['www.facebook.com', 'linkedin.com', 'instagram.com', 'youtube.com', 'tel', 'twitter.com'];
+//     const blockedPaths = ['/tel:'];
+
+//     const mainPage = await browser.newPage();
+//     await mainPage.goto(url, {
+//       waitUntil: 'domcontentloaded',
+//     });
+
+//     const pageLinks = await mainPage.evaluate(() => {
+//       const links = Array.from(document.querySelectorAll('a'));
+//       return links.map(link => link.href);
+//     });
+
+//     for (const link of pageLinks) {
+//       if (
+//         !visitedLinks.has(link) &&
+//         !blockedDomains.some(domain => link.includes(domain)) &&
+//         !blockedPaths.some(path => link.includes(path)) &&
+//         !link.includes('#') &&   // Exclude URLs with fragments
+//         !/\b\d{10,}\b/.test(link) // Exclude URLs with phone numbers
+//       ) {
+//         visitedLinks.add(link);
+//         console.log(link)
+//         if (link.toLowerCase().endsWith('.pdf')) {
+//           pdfLinks.add(link);
+//         } else if (link.startsWith('http') || link.startsWith('www')) {
+//           let newPage;
+
+//           try {
+//             newPage = await browser.newPage();
+//             await newPage.goto(link, {
+//               waitUntil: 'domcontentloaded',
+//             });
+
+//             const normalizedNewPageURL = new URL(newPage.url());
+//             const normalizedLink = new URL(link);
+
+//             if (normalizedNewPageURL.href === normalizedLink.href) {
+//               const pagePdfLinks = await newPage.evaluate(() => {
+//                 const links = Array.from(document.querySelectorAll('a[href$=".pdf"]'));
+//                 return links.map(link => link.href);
+//               });
+
+//               for (const pdfLink of pagePdfLinks) {
+//                 pdfLinks.add(pdfLink);
+//               }
+//             }
+//           } catch (error) {
+//             console.error(`Error while processing link ${link}:`, error);
+//           } finally {
+//             if (newPage) {
+//               await newPage.close();
+//             }
+//           }
+//         }
+//       }
+//     }
+
+//     const pdfLinksArray = Array.from(pdfLinks);
+
+//     if (pdfLinksArray.length > 0) {
+//       res.json({ pdfLinks: pdfLinksArray });
+//     } else {
+//       res.json({ message: 'No PDF links found.' });
+//     }
+
+//     await browser.close();
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).json({ error: 'An error occurred while scraping.' });
+//   }
+// };
+
+
+
+import axios from "axios";
+import cheerio from "cheerio";
 
 export const scrapePdfsController = async (req, res) => {
-  const { url } = req.body;
-  console.log("url", url);
-
   try {
-    const browser = await puppeteer.launch();
-    const visitedLinks = new Set();
-    const pdfLinks = new Set();
+    const { url } = req.body;
 
-    const blockedDomains = ['www.facebook.com', 'linkedin.com', 'instagram.com', 'youtube.com', 'tel', 'twitter.com'];
-    const blockedPaths = ['/tel:'];
+    // Validate the URL
+    if (!url || typeof url !== 'string') {
+      return res.status(400).json({ error: 'Invalid URL' });
+    }
 
-    const mainPage = await browser.newPage();
-    await mainPage.goto(url, {
-      waitUntil: 'domcontentloaded',
-    });
+    // Make an HTTP request to the specified URL
+    const response = await axios.get(url);
 
-    const pageLinks = await mainPage.evaluate(() => {
-      const links = Array.from(document.querySelectorAll('a'));
-      return links.map(link => link.href);
-    });
+    if (response.status === 200) {
+      const $ = cheerio.load(response.data);
+      const baseUrl = new URL(url);
+      const pdfRegex = /\.pdf$/i; // Regular expression to match PDF links
+      const pdfLinks = [];
 
-    for (const link of pageLinks) {
-      if (
-        !visitedLinks.has(link) &&
-        !blockedDomains.some(domain => link.includes(domain)) &&
-        !blockedPaths.some(path => link.includes(path)) &&
-        !link.includes('#') &&   // Exclude URLs with fragments
-        !/\b\d{10,}\b/.test(link) // Exclude URLs with phone numbers
-      ) {
-        visitedLinks.add(link);
-        console.log(link)
-        if (link.toLowerCase().endsWith('.pdf')) {
-          pdfLinks.add(link);
-        } else if (link.startsWith('http') || link.startsWith('www')) {
-          let newPage;
-
-          try {
-            newPage = await browser.newPage();
-            await newPage.goto(link, {
-              waitUntil: 'domcontentloaded',
-            });
-
-            const normalizedNewPageURL = new URL(newPage.url());
-            const normalizedLink = new URL(link);
-
-            if (normalizedNewPageURL.href === normalizedLink.href) {
-              const pagePdfLinks = await newPage.evaluate(() => {
-                const links = Array.from(document.querySelectorAll('a[href$=".pdf"]'));
-                return links.map(link => link.href);
-              });
-
-              for (const pdfLink of pagePdfLinks) {
-                pdfLinks.add(pdfLink);
-              }
-            }
-          } catch (error) {
-            console.error(`Error while processing link ${link}:`, error);
-          } finally {
-            if (newPage) {
-              await newPage.close();
-            }
-          }
+      // Extract unique links using Cheerio selectors
+      $("a").each((index, element) => {
+        const link = $(element).attr("href");
+        if (link && link.match(pdfRegex)) {
+          const absoluteLink = new URL(link, baseUrl);
+          pdfLinks.push(absoluteLink.href);
         }
-      }
-    }
+      });
 
-    const pdfLinksArray = Array.from(pdfLinks);
-
-    if (pdfLinksArray.length > 0) {
-      res.json({ pdfLinks: pdfLinksArray });
+      res.status(200).json({ pdfLinks });
     } else {
-      res.json({ message: 'No PDF links found.' });
+      res.status(response.status).json({ error: "Failed to fetch the page" });
     }
-
-    await browser.close();
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred while scraping.' });
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
